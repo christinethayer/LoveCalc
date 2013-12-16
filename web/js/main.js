b930109536285b7d6ec15540d75cf3f7dabfb65f
Christine Thayer <christine@kik.com>
2013-12-16 11:00:12 -0500

    Pretty formatting

--- web/js/main.js
+++ web/js/main.js
@@ -7,42 +7,6 @@
 	console.log("Main Page");
 	App.load('home');
 
-	App.populator('home', function (page) {
-		// put stuff here
-		console.log("testing I'm on the home page");
-		$(page).find('#You').clickable().on('click', function () {
-			App.load('You');
-		 });
-		$(page).find('#Friends').clickable().on('click', function () {
-			App.load('Friends');
-		 });
-
-		$(page).find('#People').clickable().on('click', function () {
-			App.load('People');
-		 });
-
-	});
-
-	App.populator('You', function (page) {
-		// put stuff here
-		console.log("testing you");
-	});
-	App.populator('Friends', function (page) {
-		// put stuff here
-		console.log("testing friends");
-	});
-	App.populator('People', function (page) {
-		// put stuff here
-		console.log("testing people");
-		console.log(page);
-		console.log($(page).find('#user1'));
-		console.log($(page).find('#user2'));
-	});
-	App.populator('Calc', function (page) {
-		// put stuff here
-		console.log("testing calc");
-	});
-
 	try {
 		App.restore();
 	} catch (err) {
