2960288e5166e56429a87bcf787763225e4ad178
Christine Thayer <christine@kik.com>
2013-12-18 11:54:15 -0500

    don't accept empty string as a name

--- web/js/People.js
+++ web/js/People.js
@@ -1,31 +1,22 @@
 App.populator('People', function (page) {
-	// put stuff here
 	var user1 = new Array();
 	var user2 = new Array();
 	
 	user1 = $(page).find("#user1");
 	user2 = $(page).find("#user2");
-	//$(page).find('#calc').addClass('disabled');
 
-	if (user1 === undefined || user2 === undefined){
-		//$(page).find('#calc').addClass('disabled');
-		$(page).find('#calc').click(promptUser);
-		
-		function promptUser(){
-			console.log("warning incoming game...");
-			// App.dialog{
-			// 	title        : 'Missing User' ,
-			// 	text         : 'Looks like you forgot a user...' ,
-			// 	okButton     : 'Try Again',
+	$(page).find('#calc').on('click', function (){
+		console.log(user1);
+		console.log(user1);
+
+		if(user1[0].value === "" || user1[0].value === "" ){
+			App.dialog ({title : 'Unable to Calculate', text : 'Names must be longer than 1 character', okButton : 'OK'});
 		}
-	}
-	else{
-		//$(page).find('.disabled').removeClass('disabled');
-
-		$(page).find('#calc').click(callCalc2);
-		function callCalc2(){
+		else{
 			App.load('Calc', {'user1' : user1, 'user2' : user2});
 			App.removeFromStack(1);
 		}
-	}
+
+	});
+
 });
