a2948e8a5cd21e5ac2e346e03aea139e6e87b2f8
Christine Thayer <christine@kik.com>
2013-12-31 12:53:16 -0500

    test

--- web/js/home.js
+++ web/js/home.js
@@ -1,7 +1,6 @@
 App.populator('home', function(page, user) {
 
 	$(page).find('#You').clickable().on('click', function() {
-		_gaq.push(['_trackEvent', 'You Button', 'You']);
 		App.load('You', user);
 	});
 	$(page).find('#Friends').clickable().on('click', function() {
